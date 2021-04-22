681eab0a5c1d35de762be991ca81a7954fc557c4
Elliot Stjernqvist <elliot.stjernqvist@gmail.com>
2021-04-22 11:45:50 +0200

    Code for control of robot arm with joystick

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,98 @@
 #include <Arduino.h>
 #include <Stepper.h> 
 
-int numberofstep = 4096;
-const int openPin = 12;
-const int closePin = 8;
+  int JoyStick_X = 0; // x	
+  int JoyStick_Y = 1; // y	
+  int JoyStick_Z = 13; // key
+  int x_fix = -4; // x offset fix
+  int y_fix = -12; // y offset fix
+  int JoyStick_X2 = 2; // x2
+  int JoyStick_Y2 = 3; // y2
+  int JoyStick_Z2 = 12; // key2
+  int x2_fix = 0; // x2 offset fix
+  int y2_fix = 0 ; // y2 offset fix
 
-                                  
-Stepper motor(numberofstep,9,11,10,6);    // we use pins 9, 11, 10, 6        
-void setup() 
-{ 
-  motor.setSpeed(4); // we set motor speed at 9
-  pinMode(openPin, INPUT);
-  digitalWrite(openPin, HIGH);
-  pinMode(closePin, INPUT);
-  digitalWrite(closePin, HIGH);
+  int SmallStep = 4096;
+  int BigStep = 200;
+
+  Stepper bm1(BigStep,46,47,8,49); // Arm A
+  Stepper bm2(BigStep,50,51,52,53); // Arm B
+  Stepper sm1(SmallStep,22,24,26,28); // Arm C
+  Stepper sm2(SmallStep,23,25,27,29); // Klo
+  Stepper sm3(SmallStep,30,31,32,33); // Rotation
+
+void setup() {
+  pinMode (JoyStick_X, INPUT);
+  pinMode (JoyStick_Y, INPUT);
+  pinMode (JoyStick_Z, INPUT);
+  pinMode (JoyStick_X2, INPUT);
+  pinMode (JoyStick_Y2, INPUT);
+  pinMode (JoyStick_Z2, INPUT);
+
+  bm1.setSpeed(4);
+  bm2.setSpeed(4);
+  sm1.setSpeed(4);
+  sm2.setSpeed(4);
+  sm3.setSpeed(4);
+
+  Serial.begin (9600); // 9600 bps}
 }
-void loop() 
-{ 
-  if (digitalRead(openPin) == LOW)
-  {
-    motor.step(20);
-  }
-  else if (digitalRead(closePin) == LOW)
-  {
-    motor.step(-20);
+
+void loop() {
+  int x, y, z;
+  int x2, y2, z2;
+  
+  x = analogRead (JoyStick_X) - 511 - x_fix;
+  y = analogRead (JoyStick_Y) - 511 - y_fix;
+  z = digitalRead (JoyStick_Z);
+  x2 = analogRead (JoyStick_X2) - 511 - x2_fix;
+  y2 = analogRead (JoyStick_Y2) - 511 - y2_fix;
+  z2 = digitalRead (JoyStick_Z2);
+  
+  
+  if (z == 0) {
+    Serial.println("Z-På");
+    // Öppna klo
+    sm2.step(20);
   }
   
-
+  if (x < -10) {Serial.print (x, DEC);Serial.println(" X-Ner");
+    // Arm-A bakåt
+    bm1.step(-1);
+  }
+  
+  if (x > 10) {Serial.print (x, DEC);Serial.println(" X-Upp");
+    // Arm-A framåt
+    bm1.step(1);
+  }
+  
+  if (y < -10) {Serial.print (y, DEC);Serial.println(" Y-Vänster");
+    // Vänster rotation
+    sm3.step(-20);
+  }
+  
+  if (y > 10) {Serial.print (y, DEC);Serial.println(" Y-Höger");
+    // Höger rotation
+    sm3.step(20);
+  }
+  
+  if (z2 == 0) {Serial.println("Z-På");
+    // Stäng klo
+    sm2.step(-20);}
+  
+  if (x2 < -10) {Serial.print (x, DEC);Serial.println(" X-Ner");
+    // Arm-B Ner
+  bm2.step(-1); }
+  
+  if (x2 > 10) {Serial.print (x, DEC);Serial.println(" X-Upp");
+    // Arm-B Upp
+  bm2.step(1); }
+  
+  if (y2 < -10) {Serial.print (y, DEC);Serial.println(" Y-Vänster");
+    // Arm-C Ner
+  sm1.step(-20);}
+  
+  if (y2 > 10) {Serial.print (y, DEC);Serial.println(" Y-Höger");
+    // Arm-C Upp
+  sm1.step(20); }
 }
